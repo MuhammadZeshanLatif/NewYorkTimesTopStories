a38522b2287cce7a5b783fd7585fb2909fec8d74
Muhammad Zeeshan Latif <zeeshanmuhammad42201@gmail.com>
2023-11-01 00:26:25 +0500

    Change location

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { NewsCard } from './assets/Components/cards';
+import { NewsCard } from './Components/cards';
 
 const App = () => {
   const [news, setNews] = useState([]);
