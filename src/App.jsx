e70ca81145df9bf19388c7490cd19c0be9df55d3
Muhammad Zeeshan Latif <zeeshanmuhammad42201@gmail.com>
2023-11-01 00:36:05 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { NewsCard } from './Components/cards';
+import { NewsCard } from './Components/cards.jsx';
 
 const App = () => {
   const [news, setNews] = useState([]);
